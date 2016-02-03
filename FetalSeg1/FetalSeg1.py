6b794f84bf5b2fda88c159befbbd8b0bb85f7e55
Nikhil Narayan <nikhilnarayans@gmail.com>
2016-02-04 00:43:49 +0530

    Complete algorithm

--- FetalSeg1/FetalSeg1.py
+++ FetalSeg1/FetalSeg1.py
@@ -1,8 +1,13 @@
 from skimage import io, filters, exposure
-from skimage.morphology import disk
+from skimage.morphology import disk, dilation, remove_small_objects
+from skimage.measure import regionprops, label
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 import numpy as np
 
-pInputImage = io.imread("F:\\ISBI_Challenge\\FetalSeg1\\femur-01.jpg")
+pInputImage = io.imread("F:\\ISBI_Challenge\\FetalSeg1\\femur-01.jpg",as_grey=True)
+
+pOriginalImage = pInputImage
 
 pMedFiltImg = filters.median(pInputImage,disk(5))
 
@@ -26,5 +31,27 @@ nThre = H.argmax(1)
 
 pInputImage[pInputImage<nThre] = 0
 pInputImage[pInputImage>=nThre] = 255
-io.imshow(pInputImage)
+
+pInputImage = dilation(pInputImage,disk(2))
+
+
+[pInputImage,nLabel] = label(pInputImage,connectivity=2,return_num=True)
+
+pInputImage = remove_small_objects(pInputImage,min_size=300,in_place=True)
+
+pInputImage[pInputImage>0] = 255
+
+[pInputImage,nLabel] = label(pInputImage,connectivity=1,return_num=True)
+
+
+props = regionprops(pInputImage)
+
+pRatio = [abs(float(props[i].bbox[2])-float(props[i].bbox[0]))/abs(float(props[i].bbox[3])-float(props[i].bbox[1])) for i in xrange(0,nLabel-1)]
+
+nSleekLabel = np.argmin(pRatio)
+
+pInputImage[pInputImage!=nSleekLabel+1] = 0
+pInputImage[pInputImage>0] = 255
+
+pImages = io.imshow(pInputImage)
 io.show()
