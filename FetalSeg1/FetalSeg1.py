e1bad19f20c4a2e66029759fa1f01e58579b6f31
Nikhil Narayan <nikhilnarayans@gmail.com>
2016-02-03 22:51:21 +0530

    After calculation of the entropies

--- FetalSeg1/FetalSeg1.py
+++ FetalSeg1/FetalSeg1.py
@@ -8,11 +8,21 @@ pMedFiltImg = filters.median(pInputImage,disk(5))
 
 pTotalPix = pInputImage.size
 
-pHist = np.array(exposure.histogram(pMedFiltImg,nbins=256))
-pHist = pHist.astype(float)
-pProb = pHist[0] / pTotalPix
+pHist = np.array(exposure.histogram(pMedFiltImg,nbins=256)).astype(float)
 
-print pProb
+pProb = (pHist[0] / pTotalPix) + np.spacing(1)
+
+pHProb = -pProb * np.log10(pProb)
+
+H = np.empty([1,255],dtype=float)
+for i in xrange(0,255,1):
+    HA = sum(pHProb[0:i])
+    HB = sum(pHProb[i+1:256])
+    PA = sum(pProb[0:i])+ np.spacing(1)
+    PB = sum(pProb[i+1:256]) + np.spacing(1)
+    H[0,i] = -np.log10(PA)-np.log10(PB)-(HA/(PA))-(HB/(PB))
+
+print H.argmin(1)
 #io.imshow(pInputImage)
 #io.show()
 #io.imshow(pMedFiltImg)
